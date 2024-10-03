4754f59c89bd5ab9100a42771c74cb13d6629ad4
Nugroho Ardi Sutrisno <nugroho@prospace.io>
2024-10-03 15:46:10 +0700

    tidy up for documentation

--- app/page.js
+++ app/page.js
@@ -1,6 +1,4 @@
 "use client";
-
-import Head from "next/head";
 import { useState, useEffect } from "react";
 import useStoragePermission from "./useStoragePermission";
 
@@ -23,25 +21,6 @@ export default function Home() {
 
   const { askForPermission } = useStoragePermission();
 
-  useEffect(() => {
-    document.cookie =
-      "onLoadCookie=main; max-age=3153600000; Secure; SameSite=None";
-  }, []);
-
-  const handleAddCookieButtonClick = () => {
-    // const cookieValue = 'foo'; // Set the cookie value to 'foo'
-    // document.cookie = `myCookie=${cookieValue}; path=/; expires=${new Date(new Date().getTime() + 3600000).toUTCString()}`;
-    document.cookie =
-      "onClickCookie=main; max-age=3153600000; Secure; SameSite=None";
-  };
-
-  const handleAddCookieButtonClick2 = () => {
-    // const cookieValue = 'foo'; // Set the cookie value to 'foo'
-    // document.cookie = `myCookie=${cookieValue}; path=/; expires=${new Date(new Date().getTime() + 3600000).toUTCString()}`;
-    document.cookie =
-      "set-cookie: your=cookie; Domain=phenomenal-lollipop-87c933.netlify.app; Path=/; Expires=Thu, 22 Dec 2027 04:17:44 GMT; Secure; SameSite=None";
-  };
-
   return (
     <div>
       hello
@@ -52,13 +31,8 @@ export default function Home() {
           <h2 style={{ color: "red", fontSize: "1.2em" }}>
             cookies permission testing
           </h2>
-          {/* <p>needPermission : {needPermission ? "true" : "false"}</p>
-        <p>
-          haveCheckedPermission : {haveCheckedPermission ? "true" : "false"}
-        </p> */}
-
           <ol>
-            <li>click `Add Cookie` close `Close Tab` in this new tab.</li>
+            <li>click `Close Tab` in this new tab.</li>
             <a href="./add-cookie" target="_blank">
               click here
             </a>
@@ -82,23 +56,6 @@ export default function Home() {
             </button>
           </ol>
         </div>
-        <hr />
-        <button
-          className="btn yellow continue btn-yellow-hover"
-          style={{ padding: ".4em", margin: ".2em" }}
-          onClick={handleAddCookieButtonClick}
-        >
-          Add Cookie
-        </button>
-        <hr />
-        <button
-          className="btn yellow continue btn-yellow-hover"
-          style={{ padding: ".4em", margin: ".2em" }}
-          onClick={handleAddCookieButtonClick2}
-        >
-          Add Cookie 2
-        </button>
-        {/* <CookiesTest /> */}
       </div>
     </div>
   );
